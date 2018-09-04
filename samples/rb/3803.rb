31a12b0e7169329946054f7701a50e515ae0e253
tiagodiz <tigadiz@gmail.com>
2018-09-04 20:51:57 +0200

    Delete 3803.rb

--- samples/rb/3803.rb
+++
@@ -1,5 +0,0 @@
-def method_missing(*)
-proc{}
-end
-
-ObjectSpace.each_object{|a = 1;obj|[ObjectSpace.each_object{1&a.initialize_copy(a.length)|obj| obj}] rescue a = nil;class << a;def self.b;6;end;end;0}
